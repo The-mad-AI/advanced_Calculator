717ee38ed6de8ff014f3dde95b90ab45702d06b5
Qasem Hosseini <seqasemhose@gmail.com>
2025-09-22 16:31:19 +0330

    Delete cofe_project directory

--- cofe_project/menu.py
+++
@@ -1,8 +0,0 @@
-class MenuItem:
-    def __init__(self, name, price):
-        self.name = name
-        self.price = price
-        
-    def __repr__(self):
-        return f"{self.name} - {self.price} Toman"
-    
